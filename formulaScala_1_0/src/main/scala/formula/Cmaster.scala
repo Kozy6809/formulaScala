9fd42e53305d6388ed14408d96eec01c7316407d
Kozy6809 <wakui@kcn.jp>
2021-01-03 12:42:09 +0900

    備考マスタの変更を反映

--- formulaScala_1_0/src/main/scala/formula/Cmaster.scala
+++ formulaScala_1_0/src/main/scala/formula/Cmaster.scala
@@ -22,7 +22,18 @@ final class Cmaster {
   var pcode:Int =_
   @Column(name = "備考")
   var comment:String =_
-  override def toString = pcodeNew +" "+ pcode +" "+ comment
+  @Column(name = "備考1")
+  var comment1:String =_
+  @Column(name = "備考2")
+  var comment2:String =_
+  @Column(name = "備考3")
+  var comment3:String =_
+  @Column(name = "備考4")
+  var comment4:String =_
+  @Column(name = "備考5")
+  var comment5:String =_
+
+  override def toString = pcodeNew +" "+ pcode +" "+ comment +" "+ comment1 +" "+ comment2 +" "+ comment3 +" "+ comment4 +" "+ comment5
   override def hashCode = pcode
   override def equals(o:Any) = o match {
     case p:Cmaster => p.pcode == pcode
